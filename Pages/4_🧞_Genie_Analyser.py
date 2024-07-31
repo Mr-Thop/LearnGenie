d6beb18f78ec8716c91dcbc76a214c8cd473c5b5
Mr-Thop <karthikreddy0314@gmail.com>
2024-07-31 22:06:47 +0530

    Analyser

--- Pages/4_🧞_Genie_Analyser.py
+++ Pages/4_🧞_Genie_Analyser.py
@@ -78,9 +78,6 @@ def get_conversational_chain(question, context_pdf):
     return response.text
 
 def user_input(user_question,context):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
-    docs = new_db.similarity_search(user_question)
     chain = get_conversational_chain(user_question,context)
     st.write(chain)
     
